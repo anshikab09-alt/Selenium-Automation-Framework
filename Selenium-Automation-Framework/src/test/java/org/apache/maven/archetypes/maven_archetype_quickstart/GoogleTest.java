bd910c530a8e33ff2f8ace819e1e8491b19e2b19
anshikab09-alt <bhartianshika383@gmail.com>
2026-07-12 01:54:37 +0530

    Initial commit - Selenium Automation Framework setup

---
+++ Selenium-Automation-Framework/src/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/GoogleTest.java
@@ -0,0 +1,7 @@
+package org.apache.maven.archetypes.maven_archetype_quickstart;
+
+public class GoogleTest {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Selenium Automation Framework");
+	    }
+}
